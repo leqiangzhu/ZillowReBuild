99e87ff3c6c848718f3979a95f9ec4b22a4e7c37
David Zhu <zhuleqiang@gmail.com>
2018-11-05 16:00:27 -0800

    add wrapper,backgroud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { WrapperComponent } from './wrapper/wrapper.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    WrapperComponent
   ],
   imports: [
     BrowserModule
